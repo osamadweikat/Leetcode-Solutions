5fb9101cf40e85fad7489bf2c872038c30225b11
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-14 21:43:46 +0300

    Time: 4 ms (97.32%) | Memory: 11.9 MB (53.32%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -3,7 +3,6 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* p1 = head;
         ListNode* p2 = head;
-
         while (p2 != nullptr && p2->next != nullptr) {
             p1 = p1->next;
             p2 = p2->next->next;
