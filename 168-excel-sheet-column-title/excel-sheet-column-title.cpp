e7e98e13b8b7c7c394481d3e3f2466339f30a28b
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-14 22:36:23 +0300

    Time: 0 ms (100.00%) | Memory: 7.8 MB (23.07%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+
+        while (columnNumber > 0) {
+            columnNumber--; 
+            int rem = columnNumber % 26;
+            char c = 'A' + rem;
+            res = c + res; 
+            columnNumber /= 26;
+        }
+
+        return res;
+    }
+};
