745ffe49a5bafb1f09c8812d37959da0f7a6efdf
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-14 22:00:07 +0300

    Time: 34 ms (77.23%) | Memory: 18.6 MB (23.92%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -13,11 +13,12 @@ public:
 
         ListNode* a = headA;
         ListNode* b = headB;
-
+        
         while (a != b) {
             a = (a == nullptr) ? headB : a->next;
             b = (b == nullptr) ? headA : b->next;
         }
+
         return a;
     }
 };
