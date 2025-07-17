15add3a1c5b460f0391c8275d02d0f1a2a831cea
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-18 01:49:46 +0300

    Time: 348 ms (87.64%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,4 +1,5 @@
-SELECT e1.name AS Employee
-FROM Employee e1
-JOIN Employee e2 ON e1.managerId = e2.id
-WHERE e1.salary > e2.salary;
+SELECT e2.name as Employee
+FROM employee e1
+INNER JOIN employee e2 ON e1.id = e2.managerID
+WHERE
+e1.salary < e2.salary
