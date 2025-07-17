59971285fa6070fa477662cffaac88fa3f5a3ec2
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-18 01:21:14 +0300

    Time: 0 ms (100.00%) | Memory: 28.2 MB (26.56%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int element = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                element = num;
+            }
+            count += (num == element) ? 1 : -1;
+        }
+
+        return element;
+    }
+};
