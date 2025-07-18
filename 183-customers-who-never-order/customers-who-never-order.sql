7a86b5e4c10c181814575bff7ff7605bf4d8a9f5
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-18 18:23:45 +0300

    Time: 517 ms (79.00%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,2 @@
-SELECT c.name AS Customers 
-FROM Customers c LEFT JOIN Orders o 
-ON c.id=o.customerId 
-WHERE o.customerId IS NULL;
+SELECT name AS Customers FROM Customers WHERE Customers.id 
+NOT IN (SELECT CustomerId FROM Orders);
