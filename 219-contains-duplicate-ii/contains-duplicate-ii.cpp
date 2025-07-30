0d3949febd3e33adde72ede442460410ec1cf041
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-31 01:19:04 +0300

    Time: 79 ms (37.71%) | Memory: 104.9 MB (5.11%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,13 +1,17 @@
-class Solution {
-public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int> window;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (window.count(nums[i])) return true;
-            window.insert(nums[i]);
-            if (window.size() > k) {
-                window.erase(nums[i - k]);
+class Solution
+{
+  public: 
+    bool containsNearbyDuplicate(vector<int>& nums, int k)
+    {
+        unordered_map<int, size_t> map;
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            if (map.count(nums[i]))
+            {
+                if (i - map[nums[i]] <= k)
+                    return true;
             }
+            map[nums[i]] = i;
         }
         return false;
     }
