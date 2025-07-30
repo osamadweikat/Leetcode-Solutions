b1a9b42cf064b605577ea46ec10221a722a26d24
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-31 01:20:28 +0300

    Time: 79 ms (37.71%) | Memory: 96.5 MB (68.42%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,8 +4,7 @@ public:
         unordered_map<int, int> lastIndex;
         lastIndex.reserve(nums.size());
         for (int i = 0; i < nums.size(); ++i) {
-            if (lastIndex.count(nums[i]) && i - lastIndex[nums[i]] <= k)
-                return true;
+            if (lastIndex.count(nums[i]) && i - lastIndex[nums[i]] <= k)return true;
             lastIndex[nums[i]] = i;
         }
         return false;
