0232bade3edeb82e18149f92bc85409a4546af77
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-31 01:01:15 +0300

    Time: 51 ms (57.67%) | Memory: 91 MB (31.81%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,6 +4,7 @@ public:
         unordered_set<int> seen;
         for (int num : nums) {
             if (seen.count(num)) return true;
+           
             seen.insert(num);
         }
         return false;
