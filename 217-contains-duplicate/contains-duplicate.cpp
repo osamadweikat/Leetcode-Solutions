9beba332f46276b00854b4bd5a9a69933d93f187
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-31 01:12:23 +0300

    Time: 19 ms (94.34%) | Memory: 71 MB (79.51%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
         sort(nums.begin(), nums.end());
         return unique(nums.begin(), nums.end()) != nums.end();
     }
