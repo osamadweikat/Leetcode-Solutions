1a48deb9dcc1eb3ff9252743a2449f84fccad572
Osama Dweikat <159945720+osamadweikat@users.noreply.github.com>
2025-07-31 01:05:48 +0300

    Time: 43 ms (74.58%) | Memory: 90.8 MB (46.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,10 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> seen;
         for (int num : nums) {
-            if (seen.count(num)) return true;
+            if (seen.count(num) > 0)
+                return true;
             seen.insert(num);
         }
-        
         return false;
     }
 };
